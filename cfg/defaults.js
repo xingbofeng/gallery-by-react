08fa08bdb4d153706bc14d4f55ae937e51a074a3
FE_XingBofeng <encounterxingbofeng@outlook.com>
2016-10-29 22:48:46 +0800

    解决github pages

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -58,7 +58,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: 'gallery-by-react/assets/',
+  publicPath: 'assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
